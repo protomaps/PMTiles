8c2adf666c36c4f9fef06b41e50a62e17f0491b8
Brandon Liu <bdon@bdon.org>
2021-02-16 19:51:37 +0800

    python package setup.py

---
+++ python/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pmtiles",
+    version="0.0.1",
+    author="Brandon Liu",
+    author_email="brandon@protomaps.com",
+    description="Library and utilities to write and read PMTiles files - cloud-optimized archives of map tiles.",
+    license="BSD-3-Clause",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/protomaps/pmtiles",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['bin/mbtiles2pmtiles','bin/pmtiles-show'],
+    requires_python='>=3.0'
+)

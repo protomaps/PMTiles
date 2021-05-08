e5dd54b0a813f927c243dbfdc86168e93e2a1965
Brandon Liu <bdon@bdon.org>
2021-05-08 13:39:39 +0800

    0.0.5

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pmtiles",
-    version="0.0.4",
+    version="0.0.5",
     author="Brandon Liu",
     author_email="brandon@protomaps.com",
     description="Library and utilities to write and read PMTiles files - cloud-optimized archives of map tiles.",

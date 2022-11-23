0e8d0519333eabb3f0e89fd75f58c1fd125bc646
Brandon Liu <bdon@bdon.org>
2022-11-23 10:27:08 +0800

    add check_examples CI script

---
+++ .github/check_examples.py
@@ -0,0 +1,23 @@
+import json
+import glob
+import re
+
+for package in glob.glob("**/package.json",recursive=True):
+	if "node_modules" in package:
+		continue
+
+	with open(package,"r") as f:
+		j = json.loads(f.read())
+		name = j["name"]
+		version = j["version"]
+
+	for html in glob.glob("**/*.html",recursive=True):
+		if "node_modules" in html:
+			continue
+
+		with open(html,"r") as f:
+			matches = re.findall("https://unpkg.com/" + name + r"@(\d+\.\d+\.\d+|latest)/",f.read())
+			for match in matches:
+				if matches[0] == "latest" or matches[0] != version:
+					print(html,"should be",version,"was",matches)
+					exit(1)

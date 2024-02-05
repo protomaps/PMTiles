fcc43250c36f5fbe9c705f6e1e93b9123212cfb8
Brandon Liu <bdon@bdon.org>
2024-02-05 16:56:46 +0800

    lint extra files

--- serverless/aws/src/aws_region.test.ts
+++ serverless/aws/src/aws_region.test.ts
@@ -1,37 +1,37 @@
-import { test } from "node:test";
 import assert from "node:assert";
+import { test } from "node:test";
 
-import { get_region } from "./aws_region";
+import { getRegion } from "./aws_region";
 
 test("one bucket", () => {
-  let result = get_region(
+  const result = getRegion(
     "us-west-1",
     { bucket: "mybucket", region: "us-west-1" },
-    [],
+    []
   );
   assert.deepEqual(result, { bucket: "mybucket", region: "us-west-1" });
 });
 
 test("unknown region", () => {
-  let result = get_region(
+  const result = getRegion(
     "us-nullisland-1",
     { bucket: "mybucket", region: "us-west-1" },
-    [],
+    []
   );
   assert.deepEqual(result, { bucket: "mybucket", region: "us-west-1" });
 });
 
 test("exact region match", () => {
-  let result = get_region(
+  let result = getRegion(
     "us-west-1",
     { bucket: "mybucket", region: "us-west-1" },
-    [{ bucket: "mybucket-ap-south-1", region: "ap-south-1" }],
+    [{ bucket: "mybucket-ap-south-1", region: "ap-south-1" }]
   );
   assert.deepEqual(result, { bucket: "mybucket", region: "us-west-1" });
-  result = get_region(
+  result = getRegion(
     "ap-south-1",
     { bucket: "mybucket", region: "us-west-1" },
-    [{ bucket: "mybucket-ap-south-1", region: "ap-south-1" }],
+    [{ bucket: "mybucket-ap-south-1", region: "ap-south-1" }]
   );
   assert.deepEqual(result, {
     bucket: "mybucket-ap-south-1",
@@ -40,10 +40,10 @@ test("exact region match", () => {
 });
 
 test("priority match", () => {
-  let result = get_region(
+  const result = getRegion(
     "us-west-1",
     { bucket: "mybucket", region: "ap-south-1" },
-    [{ bucket: "mybucket-us-west-2", region: "us-west-2" }],
+    [{ bucket: "mybucket-us-west-2", region: "us-west-2" }]
   );
   assert.deepEqual(result, {
     bucket: "mybucket-us-west-2",

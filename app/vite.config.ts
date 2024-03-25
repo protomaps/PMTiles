71e83f138a6f25e44624e763f9ae064f38492828
Brandon Liu <bdon@users.noreply.github.com>
2024-03-25 13:35:36 -0700

    Fix tileinspect entrypoint in gh pages build [#49, #379] (#380)

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        mapview: resolve(__dirname, "index.html"),
+        tileinspect: resolve(__dirname, "tileinspect/index.html")
+      },
+    },
+  },
+});

b86d9e7548e0d76ea5b83cf78e5dc2cb96ca6bbb
Brandon Liu <bdon@bdon.org>
2024-09-02 15:00:47 +0800

    include both build-zip and build-cloudformation-stack in CI

--- serverless/aws/inline_lambda.ts
+++ serverless/aws/inline_lambda.ts
@@ -4,4 +4,4 @@ import fs from 'fs';
 const template = yamlParse(fs.readFileSync('protomaps-template.yaml','utf-8'));
 const code = fs.readFileSync('dist/index.js','utf8');
 template.Resources.LambdaFunction.Properties.Code = {ZipFile:code};
-fs.writeFileSync('out.yaml', yamlDump(template));
+fs.writeFileSync('dist/cloudformation-stack.yaml', yamlDump(template));

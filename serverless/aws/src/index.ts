3f320aeda0a7adb391f9788d5b09f470775e9af4
Brandon Liu <bdon@users.noreply.github.com>
2024-09-04 11:34:44 +0800

    AWS Lambda: update dependencies (#439)
    * fix fast-xml-parser security issue.
    * update types to nodejs20
    * replace deprecated @aws-sdk/node-http-handler with @smithy/node-http-handler, which requires bundling as it's not included in the Lambda runtime.

--- serverless/aws/src/index.ts
+++ serverless/aws/src/index.ts
@@ -22,7 +22,7 @@ import {
   GetObjectCommandOutput,
   S3Client,
 } from "@aws-sdk/client-s3";
-import { NodeHttpHandler } from "@aws-sdk/node-http-handler";
+import { NodeHttpHandler } from "@smithy/node-http-handler";
 
 // the region should default to the same one as the function
 const s3client = new S3Client({

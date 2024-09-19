1d9f955a5582620ea02424c5b64cd6ebf6160a8f
Brandon Liu <bdon@bdon.org>
2024-09-19 18:45:34 +0800

    lint

--- js/index.ts
+++ js/index.ts
@@ -368,7 +368,7 @@ export class FetchSource implements Source {
     this.mustReload = false;
     let userAgent = "";
     if ("navigator" in globalThis) {
-      //biome-ignore lint
+      //biome-ignore lint: cf workers
       userAgent = (globalThis as any).navigator.userAgent;
     }
     const isWindows = userAgent.indexOf("Windows") > -1;

97f2dfdd9e171b510274ccdc20e228b65fd1ba7f
Brandon Liu <bdon@bdon.org>
2025-02-05 12:59:30 +0800

    more skeleton code

--- app2/src/index.tsx
+++
@@ -1,8 +0,0 @@
-/* @refresh reload */
-import { render } from 'solid-js/web'
-import './index.css'
-import App from './App.tsx'
-
-const root = document.getElementById('root')
-
-render(() => <App />, root!)

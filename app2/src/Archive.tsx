1c45732fd8cb8c668d55591c65fa249a3a4cc593
Brandon Liu <bdon@bdon.org>
2025-02-05 12:59:30 +0800

    more skeleton code

---
+++ app2/src/Archive.tsx
@@ -0,0 +1,17 @@
+/* @refresh reload */
+import { render } from "solid-js/web";
+import './index.css'
+
+function Archive() {
+  return (
+    <>
+      <h1 class="text-xl">Vite + Solid</h1>
+    </>
+  )
+}
+
+const root = document.getElementById("root");
+
+if (root) {
+  render(() => <Archive />, root);
+}

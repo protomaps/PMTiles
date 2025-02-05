61815fb67767af189c3bcb0c3075f80b1a907b04
Brandon Liu <bdon@bdon.org>
2025-02-05 12:59:30 +0800

    more skeleton code

---
+++ app2/src/Map.tsx
@@ -0,0 +1,17 @@
+/* @refresh reload */
+import { render } from "solid-js/web";
+import './index.css'
+
+function Map() {
+  return (
+    <>
+      <h1 class="text-xl">Vite + Solid</h1>
+    </>
+  )
+}
+
+const root = document.getElementById("root");
+
+if (root) {
+  render(() => <Map />, root);
+}
--- app2/vite.config.ts
+++ app2/vite.config.ts
@@ -1,6 +1,17 @@
+import { resolve } from "node:path";
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        map: resolve(__dirname, "index.html"),
+        archive: resolve(__dirname, "archive/index.html"),
+        tile: resolve(__dirname, "tile/index.html"),
+      },
+    },
+  },
 })

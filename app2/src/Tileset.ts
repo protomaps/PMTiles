98599db52844ebd78ee6389303c1bdfc6f060662
Brandon Liu <bdon@bdon.org>
2025-02-05 23:29:23 +0800

    Stuff is working

--- app2/src/Tileset.ts
+++ app2/src/Tileset.ts
@@ -1,2 +1,10 @@
 // a TileJSON or a .pmtiles archive, local or remote
 // gets metadata, tiles, etc
+
+export class Tileset {
+  url: string;
+
+  constructor(url: string) {
+    this.url = url;
+  }
+}

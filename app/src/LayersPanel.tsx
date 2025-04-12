98f84c32d043649f40e01b2b41f537c2580154b7
Brandon Liu <bdon@bdon.org>
2025-04-12 16:51:24 +0800

    checkboxes

--- app/src/LayersPanel.tsx
+++ app/src/LayersPanel.tsx
@@ -1,4 +1,11 @@
-import { For, type Setter, Show, createResource } from "solid-js";
+import {
+  For,
+  type Setter,
+  Show,
+  createEffect,
+  createResource,
+  createMemo,
+} from "solid-js";
 import type { Tileset } from "./tileset";
 import { colorForIdx } from "./utils";
 
@@ -12,16 +19,61 @@ export function LayersPanel(props: {
   setLayerVisibility: Setter<LayerVisibility[]>;
   layerFeatureCounts?: Record<string, number>;
 }) {
+  let checkallRef;
+
+  const onChange = (id: string) => {
+    const newLayerVisibility = props
+      .layerVisibility()
+      .map((l) => (l.id === id ? { ...l, visible: !l.visible } : l));
+    props.setLayerVisibility(newLayerVisibility);
+  };
+
+  const allChecked = createMemo(() => {
+    const visibleLayersCount = props
+      .layerVisibility()
+      .filter((l) => l.visible).length;
+    return visibleLayersCount === props.layerVisibility().length;
+  });
+
+  const toggleAll = () => {
+    props.setLayerVisibility(
+      props.layerVisibility().map((l) => ({ ...l, visible: !allChecked() })),
+    );
+  };
+
+  createEffect(() => {
+    const visibleLayersCount = props
+      .layerVisibility()
+      .filter((l) => l.visible).length;
+    const indeterminate =
+      visibleLayersCount > 0 &&
+      visibleLayersCount !== props.layerVisibility().length;
+    if (checkallRef) {
+      checkallRef.indeterminate = indeterminate;
+    }
+  });
+
   return (
     <div class="bg-white dark:bg-gray-900 dark:text-white rounded p-4 border border-gray-700">
-      <input type="checkbox" id={"checkall"} />
-      <label class="ml-2 text-sm" for={"checkall"}>
+      <input
+        type="checkbox"
+        id="checkall"
+        ref={checkallRef}
+        checked={allChecked()}
+        onChange={toggleAll}
+      />
+      <label class="ml-2 text-sm" for="checkall">
         All Layers
       </label>
       <For each={props.layerVisibility()}>
         {(l, i) => (
           <div class="ml-2">
-            <input type="checkbox" id={`check_${l.id}`} checked={l.visible} />
+            <input
+              type="checkbox"
+              id={`check_${l.id}`}
+              checked={l.visible}
+              onChange={() => onChange(l.id)}
+            />
             <label class="ml-2 text-sm" for={`check_${l.id}`}>
               <span
                 class="inline-block mr-2 w-[10px] h-[10px]"

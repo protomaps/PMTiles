35eaacfe5e37bbc91a47e6fa11f8f1c0224131ae
Brandon Liu <bdon@users.noreply.github.com>
2026-09-11 16:08:32 -0400

    Maplibre 6 9 (#672)
    * app: upgrade maplibre-gl to 6.9
    
    * removes need for rtl-text plugin
    
    * switch default map from demo-bucket.protomaps.com to latest.protomaps.com
    
    * upgrade deps
    
    * switch overture example to mirrored divisions.pmtiles [#669]

--- app/src/Frame.tsx
+++ app/src/Frame.tsx
@@ -36,7 +36,7 @@ export const ExampleChooser = (props: {
             class="block p-2 flex text-left flex-col hover:bg-slate dark:hover:bg-purple w-full cursor-pointer"
             type="button"
             onClick={() => {
-              loadSample("https://demo-bucket.protomaps.com/v4.pmtiles");
+              loadSample("https://latest.protomaps.com/v4.pmtiles");
             }}
           >
             <div>v4.pmtiles</div>
@@ -61,13 +61,13 @@ export const ExampleChooser = (props: {
             type="button"
             onClick={() => {
               loadSample(
-                "https://overturemaps-tiles-us-west-2-beta.s3.amazonaws.com/2025-04-23/places.pmtiles",
+                "https://r2-public.protomaps.com/overture-tiles/2026-08-19.0/divisions.pmtiles",
               );
             }}
           >
-            <div>places.pmtiles</div>
+            <div>divisions.pmtiles</div>
             <div class="text-xs app-text-light">
-              vector overlay, Overture Places, 2025-04-23 release
+              vector overlay, Overture Divisions, 2026-08-19 release
             </div>
           </button>
           <button
